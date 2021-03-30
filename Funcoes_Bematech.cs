fad94c5649c537ff74038bafed3cc3d588f80153
LucasIdalgo <lucasidalgodossantos@gmail.com>
2021-03-30 09:29:04 -0300

    Implementando funções da DLL

---
+++ Funcoes_Bematech.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace MicroTerminalFIT
+{
+    public class Funcoes_Bematech
+    {
+        [DllImport("BemaSB32.dll")]
+        public static extern int Bematech_FIT_IniciaComunicacao ();
+
+        [DllImport("BemaSB32.dll")]
+        public static extern int Bematech_FIT_FechaComunicacao ();
+
+        //Parametro protocolo
+        [DllImport("BemaSB32.dll")]
+        public static extern int Bematech_FIT_SelecionaProtocolo (int protocolo);
+
+        //Parametro terminal
+        [DllImport("BemaSB32.dll")]
+        public static extern int Bematech_FIT_LimpaDisplay(byte terminal);
+
+        //Parametro terminal
+        [DllImport("BemaSB32.dll")]
+        public static extern int Bematech_FIT_EscreveDisplay(byte terminal);
+
+        //Parametros terminal, linha, coluna
+        [DllImport("BemaSB32.dll")]
+        public static extern int Bematech_FIT_PosicionaCursor(byte terminal, byte linha, byte coluna);
+
+        //Parametros terminal
+        [DllImport("BemaSB32.dll")]
+        public static extern int Bematech_FIT_LerBuffer(byte terminal);
+
+        //Parametros terminal, dados, serial
+        [DllImport("BemaSB32.dll")]
+        public static extern int Bematech_FIT_EnviaSerial(byte terminal, byte dados, byte serial);
+
+        //Parametros terminal
+        [DllImport("BemaSB32.dll")]
+        public static extern int Bematech_FIT_ApagaLinha(byte terminal);
+
+        //Parametros terminal
+        [DllImport("BemaSB32.dll")]
+        public static extern int Bematech_FIT_DeslocaCursorCima(byte terminal);
+
+        //Parametros terminal
+        [DllImport("BemaSB32.dll")]
+        public static extern int Bematech_FIT_DeslocaCursorDireita(byte terminal);
+
+        //Parametros terminal
+        [DllImport("BemaSB32.dll")]
+        public static extern int Bematech_FIT_VersaoFirmware(byte terminal);
+
+        //Parametros terminal, comando
+        [DllImport("BemaSB32.dll")]
+        public static extern int Bematech_FIT_ComandoGenerico(byte terminal, byte comando);
+
+        //Parametros terminal
+        [DllImport("BemaSB32.dll")]
+        public static extern int Bematech_FIT_LeituraCodidoBarras(byte terminal);
+
+        //Parametros comando
+        [DllImport("BemaSB32.dll")]
+        public static extern int Bematech_FIT_EnviaComando(string comando);
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MicroTerminalFIT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void IComunicacao_Click(object sender, RoutedEventArgs e)
+        {
+            int retorno;
+            retorno = Funcoes_Bematech.Bematech_FIT_IniciaComunicacao();
+
+            if (retorno == 0)
+            {
+                MessageBox.Show("Erro de iniciar comunicação");
+            }
+        }
+
+        private void FComunicacao_Click(object sender, RoutedEventArgs e)
+        {
+            int retorno;
+            retorno = Funcoes_Bematech.Bematech_FIT_FechaComunicacao();
+
+            if (retorno == 0)
+            {
+                MessageBox.Show("Erro ao fechar comunicação");
+            }
+        }
+    }
+}
