275dd9cabfd7e91f7fbe96761d752b394da181a9
LucasIdalgo <lucasidalgodossantos@gmail.com>
2021-04-07 01:39:38 -0300

    Implementando função para buscar firmware e ler buffer

--- Funcoes_Bematech.cs
+++ Funcoes_Bematech.cs
@@ -19,47 +19,47 @@ namespace MicroTerminalFIT
 
         //Parametro terminal
         [DllImport("BemaSB32.dll")]
-        public static extern int Bematech_FIT_LimpaDisplay(byte terminal);
+        public static extern int Bematech_FIT_LimpaDisplay(char terminal);
 
         //Parametro terminal
         [DllImport("BemaSB32.dll")]
-        public static extern int Bematech_FIT_EscreveDisplay(byte terminal);
+        public static extern int Bematech_FIT_EscreveDisplay(char terminal);
 
         //Parametros terminal, linha, coluna
         [DllImport("BemaSB32.dll")]
-        public static extern int Bematech_FIT_PosicionaCursor(byte terminal, byte linha, byte coluna);
+        public static extern int Bematech_FIT_PosicionaCursor(char terminal, char linha, char coluna);
 
         //Parametros terminal
         [DllImport("BemaSB32.dll")]
-        public static extern int Bematech_FIT_LerBuffer(byte terminal);
+        public static extern char Bematech_FIT_LerBuffer(char terminal);
 
         //Parametros terminal, dados, serial
         [DllImport("BemaSB32.dll")]
-        public static extern int Bematech_FIT_EnviaSerial(byte terminal, byte dados, byte serial);
+        public static extern int Bematech_FIT_EnviaSerial(char terminal, char dados, char serial);
 
         //Parametros terminal
         [DllImport("BemaSB32.dll")]
-        public static extern int Bematech_FIT_ApagaLinha(byte terminal);
+        public static extern int Bematech_FIT_ApagaLinha(char terminal);
 
         //Parametros terminal
         [DllImport("BemaSB32.dll")]
-        public static extern int Bematech_FIT_DeslocaCursorCima(byte terminal);
+        public static extern int Bematech_FIT_DeslocaCursorCima(char terminal);
 
         //Parametros terminal
         [DllImport("BemaSB32.dll")]
-        public static extern int Bematech_FIT_DeslocaCursorDireita(byte terminal);
+        public static extern int Bematech_FIT_DeslocaCursorDireita(char terminal);
 
         //Parametros terminal
         [DllImport("BemaSB32.dll")]
-        public static extern int Bematech_FIT_VersaoFirmware(byte terminal);
+        public static extern int Bematech_FIT_VersaoFirmware(char terminal);
 
         //Parametros terminal, comando
         [DllImport("BemaSB32.dll")]
-        public static extern int Bematech_FIT_ComandoGenerico(byte terminal, byte comando);
+        public static extern int Bematech_FIT_ComandoGenerico(char terminal, char comando);
 
         //Parametros terminal
         [DllImport("BemaSB32.dll")]
-        public static extern int Bematech_FIT_LeituraCodidoBarras(byte terminal);
+        public static extern int Bematech_FIT_LeituraCodidoBarras(char terminal);
 
         //Parametros comando
         [DllImport("BemaSB32.dll")]
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -31,9 +32,12 @@ namespace MicroTerminalFIT
             retorno = Funcoes_Bematech.Bematech_FIT_IniciaComunicacao();
 
             if (retorno == 0)
-            {
-                MessageBox.Show("Erro de iniciar comunicação");
-            }
+                MessageBox.Show("Erro ao iniciar comunicação","ERRO",MessageBoxButton.OK);
+
+            if (Protocolo1.IsChecked == true)
+                retorno = Funcoes_Bematech.Bematech_FIT_SelecionaProtocolo(1);
+            else
+                retorno = Funcoes_Bematech.Bematech_FIT_SelecionaProtocolo(0);
         }
 
         private void FComunicacao_Click(object sender, RoutedEventArgs e)
@@ -42,8 +46,31 @@ namespace MicroTerminalFIT
             retorno = Funcoes_Bematech.Bematech_FIT_FechaComunicacao();
 
             if (retorno == 0)
+                MessageBox.Show("Erro ao fechar comunicação", "ERRO", MessageBoxButton.OK);
+        }
+
+        private void VFirmware_Click(object sender, RoutedEventArgs e)
+        {
+            int retorno;
+            char temp='0';
+            string buffer;
+
+            retorno = Funcoes_Bematech.Bematech_FIT_VersaoFirmware(Convert.ToChar(Terminal.Text));
+
+            if (retorno == 0)
+                MessageBox.Show("Erro ao obter firmware", "ERRO", MessageBoxButton.OK);
+            else
             {
-                MessageBox.Show("Erro ao fechar comunicação");
+                buffer = "";
+
+                //Fazer o cast do char -(char)- é equivalente ao método Chr em VB
+                while (temp != (char)3) {
+                    Thread.Sleep(100);
+                    temp = Funcoes_Bematech.Bematech_FIT_LerBuffer(Convert.ToChar(Terminal.Text));
+                    buffer += temp;
+                };
+
+                InfFirmware.Items.Add(buffer);
             }
         }
     }
